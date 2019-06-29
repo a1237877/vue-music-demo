97bba8177663afa2385795ccd4a319a0a37543c5
a1237877 <487333601@qq.com>
2019-06-29 22:26:29 +0800

    1

---
+++ src/components/pages/searchList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="search-list" v-show="searches.length">
+    <transition-group name="list" tag="ul">
+      <li class="search-item" v-for="item in searches" :key="item" @click="searchItem(item)">
+        <span class="text">{{item}}</span>
+        <span class="icon-box" @click.stop="deleteOne(item)">  
+          <i class="icon">&#xe656;</i>
+        </span>
+      </li> 
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    }
+  },
+  props:{
+    searches:{
+      type:Array,
+      default:[]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.search-list 
+  .search-item 
+    display flex
+    align-items center
+    height 40px
+    overflow hidden
+
+    &.list-enter-active, &.list-leave-active 
+      transition all 0.1s
+    
+
+    &.list-enter, &.list-leave-to 
+      height 0
+    
+
+    .text 
+      flex 1
+      color hsla(0,0%,100%,.5)
+    
+
+    .icon-box 
+      .icon 
+        font-size 18px
+        color hsla(0,0%,100%,.3)
+</style>
+
---
+++ src/components/tab.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tab">
+    <router-link tag="div" class="tab-item" active-class="tab-link" to="/recommend">
+      <span class="tab-link">推荐</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" active-class="tab-link" to="/singer">
+      <span class="tab-link">歌手</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" active-class="tab-link" to="/rank">
+      <span class="tab-link">排行</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" active-class="tab-link" to="/search">
+      <span class="tab-link">搜索</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../assets/css/function"
+.tab
+  display flex
+  height px2rem(88px)
+  line-height px2rem(88px)
+  font-size 14px
+  &-item
+    flex 1
+    text-align center
+    .tab-link
+      padding-bottom 5px
+      color hsla(0, 0%, 100%, 0.5)
+    &.router-link-exact-active
+      .tab-link
+        color red
+        border-bottom 2px solid #ea2448
+</style>
